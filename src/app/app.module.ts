463497b4b28ca33fd0fe2c271554154aecf28d28
mateo-a <alexander.urrego@gmail.com>
2021-08-01 00:43:35 -0500

    [Add] - New version of activities board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskComponent } from './task/task.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCpQWgfL37081rrXf_OhYYJEy2r1DyPLvM",
+    authDomain: "activitiesboard-1d6d2.firebaseapp.com",
+    projectId: "activitiesboard-1d6d2",
+    storageBucket: "activitiesboard-1d6d2.appspot.com",
+    messagingSenderId: "267293662000",
+    appId: "1:267293662000:web:c9d20ce70f742d0f1b06f5",
+    measurementId: "G-1L1X57VPNB"
+  },
 };
